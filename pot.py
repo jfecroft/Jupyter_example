f62d493d2db9aed769c8a135c15cc14766f7ac67
jfecroft <j.f.e.croft@gmail.com>
2024-06-18 13:11:06 +0100

    add some stuff

--- pot.py
+++ pot.py
@@ -1,11 +1,11 @@
 from scipy.interpolate import CubicSpline
 from numpy import loadtxt
 
-v_kk_1 = np.loadtxt('kk_singlet')
-v_kk_3 = np.loadtxt('kk_triplet')
+v_kk_1 = loadtxt('kk_singlet')
+v_kk_3 = loadtxt('kk_triplet')
 
-v_krb_1 = np.loadtxt('krb_singlet')
-v_krb_3 = np.loadtxt('krb_triplet')
+v_krb_1 = loadtxt('krb_singlet')
+v_krb_3 = loadtxt('krb_triplet')
 
 pot_singlet = CubicSpline(v_kk_1[:, 0], v_kk_1[:, 1])
 pot_triplet = CubicSpline(v_kk_3[:, 0], v_kk_3[:, 1])
